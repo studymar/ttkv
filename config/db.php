33dab440662cb6b86fe589fee6cf2c73d7358b3e
Win10 <mworthmann@googlemail.com>
2022-03-27 16:53:57 +0200

    Prod DB

--- config/db.php
+++ config/db.php
@@ -7,6 +7,11 @@ return [
     'password' => '',
     'charset' => 'utf8',
 
+    //prod
+    'dsn' => 'mysql:host=localhost;dbname=d039508e',
+    'username' => 'd039508e',
+    'password' => '8zUBarHc6shh2vgA',
+    
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
