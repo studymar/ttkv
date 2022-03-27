3a165b73f59fd74dd276bc028c69defa274fcfbb
Win10 <mworthmann@googlemail.com>
2022-03-27 23:47:00 +0200

    Vereinsmeldung Click-tt und Pokal

---
+++ models/vereinsmeldung/confirmpokal/VereinsmeldungPokal.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace app\models\vereinsmeldung\confirmpokal;
+
+use Yii;
+use app\models\vereinsmeldung\IFIsVereinsmeldemodul;
+use app\models\vereinsmeldung\Vereinsmeldung;
+
+/**
+ * This is the model class for table "vereinsmeldung_pokal".
+ *
+ * @property int $id
+ * @property int $done
+ * @property string|null $created_at
+ * @property int $vereinsmeldung_id
+ *
+ * @property Vereinsmeldung $vereinsmeldung
+ */
+class VereinsmeldungPokal extends \yii\db\ActiveRecord implements IFIsVereinsmeldemodul
+{
+
+    private static $instance = null;
+    
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'vereinsmeldung_pokal';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'vereinsmeldung_id'], 'required'],
+            [['id', 'vereinsmeldung_id','done'], 'integer'],
+            //nur bei save, bei create kann auch 0 sein
+            [['done'], 'integer','min'=>1, 'max'=>'1',"tooSmall"=>"Bitte gib zuerst die Pokalmeldung in Click-tt ein", "on"=>"update"],
+            [['created_at'], 'safe'],
+            [['id'], 'unique'],
+            [['vereinsmeldung_id'], 'exist', 'skipOnError' => true, 'targetClass' => Vereinsmeldung::className(), 'targetAttribute' => ['vereinsmeldung_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'created_at' => 'Created At',
+            'vereinsmeldung_id' => 'Vereinsmeldung ID',
+            'done'      => 'In Click-tt eingegeben',
+        ];
+    }
+
+    /**
+     * Gets query for [[Vereinsmeldung]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getVereinsmeldung()
+    {
+        return $this->hasOne(Vereinsmeldung::className(), ['id' => 'vereinsmeldung_id']);
+    }
+
+    
+    public static function getInstance($vereinsmeldung){
+        if(self::$instance)
+            return self::$instance;
+        self::$instance = VereinsmeldungPokal::find()->where(['vereinsmeldung_id'=>$vereinsmeldung->id])->one();
+        if(!self::$instance)
+            self::$instance = VereinsmeldungPokal::create($vereinsmeldung);
+        return self::$instance;        
+    }
+
+    /**
+     * Prueft, ob schon erledigt
+     * @return boolean
+     */
+    public static function isDone($vereinsmeldung){
+        //wenn Eintrag vorhanden und mind. eine Person dazu gespeichert
+        $item = self::getInstance($vereinsmeldung);
+        if($item && $item->done)
+            return true;
+        return false;
+    }
+    
+    /**
+     * Prueft, ob ein Hinweis zur Meldung angezeigt werden kann, warum der Punkt noch nicht erledigt ist
+     * Beispiel: Nur halb ausgefüllt, was muss noch geschehen?
+     * @return string
+     */
+    public static function doneError($vereinsmeldung){
+        $item = self::getInstance($vereinsmeldung);
+        return false;
+    }
+
+    
+    /**
+     * Erstellt für eine Vereinsmeldung eine VereinsmeldungPokal
+     * @param type $vereinsmeldung
+     * @return VereinsmeldungTeams|Exception
+     */
+    public static function create(Vereinsmeldung $vereinsmeldung){
+        Yii::debug("Create VereinsmeldungClickTT for vereinsmeldung_id ".$vereinsmeldung->id, __METHOD__);
+        $item = new VereinsmeldungPokal();
+        $item->id           = 0;
+        $item->done         = 0;
+        $item->vereinsmeldung_id = $vereinsmeldung->id;
+        $item->created_at   = new \yii\db\Expression('NOW()');
+        if($item->save())
+            return $item;
+        Yii::error(\yii\helpers\Json::encode($item->getErrors()));
+        return new \yii\base\Exception(\yii\helpers\Json::encode($item->getErrors()));
+    }
+    
+    public function setdone(){
+        $this->done  = 1;
+    }    
+    
+}
