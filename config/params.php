504971a6432f32ad39e33c1edb9aced97072687f
Win10 <mworthmann@googlemail.com>
2022-05-04 21:01:18 +0200

    Domain auf vereine.ttkv-harburg.de geändert

--- config/params.php
+++ config/params.php
@@ -2,7 +2,7 @@
 
 return [
     //'domain'     => 'https://ttkv.localhost',
-    'domain'     => 'https://controlcenter.ttkv-harburg.de',
+    'domain'     => 'https://vereine.ttkv-harburg.de',
     'adminEmail' => 'info@ttkv-harburg.de',
     'senderEmail' => 'info@ttkv-harburg.de',
     'senderName' => 'TTKV Harburg-Land e.V.',
